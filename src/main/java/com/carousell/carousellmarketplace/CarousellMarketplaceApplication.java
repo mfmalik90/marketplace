20a9cd4cf60c9e33dfb33891dd2d08319c71303f
faizan.malik <faizan.malik@careem.com>
2020-03-04 21:48:00 +0400

    first commit

---
+++ src/main/java/com/carousell/carousellmarketplace/CarousellMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.carousell.carousellmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarousellMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarousellMarketplaceApplication.class, args);
+	}
+
+}
